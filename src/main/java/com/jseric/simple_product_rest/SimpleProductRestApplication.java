d4e1c2a8ce4bb5e2b5310a078f28f9653d00dc30
Josip Šerić <josip.seric@icloud.com>
2022-02-04 15:47:27 +0100

    Initialized maven project

---
+++ src/main/java/com/jseric/simple_product_rest/SimpleProductRestApplication.java
@@ -0,0 +1,12 @@
+package com.jseric.simple_product_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SimpleProductRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleProductRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jseric/simple_product_rest/SimpleProductRestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jseric.simple_product_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SimpleProductRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
