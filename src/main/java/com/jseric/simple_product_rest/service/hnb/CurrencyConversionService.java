ab83d7686aa04dabb86256ddd72379265d871c70
Josip Šerić <josip.seric@icloud.com>
2022-02-09 23:35:34 +0100

    Added HNB API communicator and CurrencyConversionService

---
+++ src/main/java/com/jseric/simple_product_rest/service/hnb/CurrencyConversionService.java
@@ -0,0 +1,41 @@
+package com.jseric.simple_product_rest.service.hnb;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CurrencyConversionService {
+    public static int INVALID_VALUE = -1;
+
+    private final HnbCommunicator hnbCommunicator;
+
+    @Autowired
+    public CurrencyConversionService(HnbCommunicator hnbCommunicator) {
+        this.hnbCommunicator = hnbCommunicator;
+    }
+
+    /**
+     * Convert value in HRK currency to price in EUR currency.
+     * Conversion rate is retrieved from HNB API.
+     * @param hrk Value in HRK currency
+     * @return Converted rate.
+     *         If HRK value is null, null is returned
+     *         If conversion rate cannot be fetched, null is returned.
+     */
+    public BigDecimal convertHrkToEur(final BigDecimal hrk) {
+        if (hrk == null) {
+            return null;
+        }
+
+        final BigDecimal conversionRate = hnbCommunicator.fetchTodaysEuroConversionRate();
+        if (conversionRate == null) {
+            // Conversion rate could not be fetched, return invalid value
+            return new BigDecimal(INVALID_VALUE);
+        }
+        return hrk.divide(conversionRate, 2, RoundingMode.HALF_EVEN);
+    }
+}
---
+++ src/main/java/com/jseric/simple_product_rest/service/hnb/HnbCommunicator.java
@@ -0,0 +1,95 @@
+package com.jseric.simple_product_rest.service.hnb;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class HnbCommunicator {
+    private static final String HNB_API_URI = "https://api.hnb.hr/tecajn/v1?";
+
+    private static final String CURRENCY_CODE_PARAM = "valuta";
+    private static final String DATE_PARAM = "datum";
+    private static final String EUR_CC = "EUR";
+
+    private static final String AVG_CONVERSION_RATE_NODE = "Srednji za devize";
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    public HnbCommunicator() {
+    }
+
+    /**
+     * Fetches today's HRK to EUR conversion rate from HNB API.
+     * @return Average conversion rate. null will be returned in case of any error.
+     */
+    public BigDecimal fetchTodaysEuroConversionRate() {
+        return fetchConversionRate(EUR_CC, null);
+    }
+
+    /**
+     * Send GET request to HNB API (https://api.hnb.hr) to get average conversion rate for desired currencyCode and date
+     * @param currencyCode Currency code
+     * @param date Date (null for today)
+     * @return Average conversion rate. null will be returned in case of any error.
+     */
+    private BigDecimal fetchConversionRate(final String currencyCode, final Date date) {
+        final StringBuilder uri = new StringBuilder();
+        uri.append(HNB_API_URI).append(CURRENCY_CODE_PARAM).append('=').append(currencyCode);
+        if (date != null) {
+            uri.append('&').append(DATE_PARAM).append('=').append(DATE_FORMAT.format(date));
+        }
+
+        log.info("Sending a GET request to " + uri);
+        final RestTemplate restTemplate = new RestTemplate();
+        final ResponseEntity<String> response = restTemplate.getForEntity(uri.toString(), String.class);
+        return parseReponseObject(response);
+    }
+
+    /**
+     * Extract average conversion rate from response object.
+     *
+     * @param response {@link org.springframework.http.ResponseEntity<String>} object
+     * @return Average conversion rate from response body.
+     *          If response status is not HTTP OK or if response body is not JSON or
+     *          if average conversion rate field is not in decimal format, null will be returned.
+     */
+    private BigDecimal parseReponseObject(ResponseEntity<String> response) {
+        log.trace("Response :: " + response);
+
+        // Check status code
+        if (!response.getStatusCode().equals(HttpStatus.OK)) {
+            // Something went wrong
+            log.info("Response code is " + response.getStatusCode());
+            return null;
+        }
+
+        String avgConversionRate = null;
+        try {
+            // Parse response body as JSON
+            final JSONArray jsonArray = new JSONArray(response.getBody());
+            avgConversionRate = jsonArray.getJSONObject(0).getString(AVG_CONVERSION_RATE_NODE);
+        } catch (final JSONException e) {
+            log.warn("There was a problem parsing response JSON: " + e.getMessage());
+            return null;
+        }
+
+        try {
+            return new BigDecimal(avgConversionRate.replaceAll(",", "."));
+        } catch (final NumberFormatException e) {
+            log.warn("Response body field for avg conversion rate does not contain a decimal number");
+            return null;
+        }
+    }
+
+}
